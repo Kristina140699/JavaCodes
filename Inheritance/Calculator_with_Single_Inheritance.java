0060b56a7d9a00a9213acb29d985f3e2e83744a6
Kristina <71969190+Kristina140699@users.noreply.github.com>
2021-02-15 15:04:16 +0530

    Update Calculator_with_Single_Inheritance.java

--- Inheritance/Calculator_with_Single_Inheritance.java
+++ Inheritance/Calculator_with_Single_Inheritance.java
@@ -1,21 +1,34 @@
-class Calculation{
-    int z;
-    public void add(int x, int y)
-    {z =x+y;
-        System.out.println("The sum of the given numbers:"+z);    }
-    public void sub(int x, int y){
-        z=x-y;
-        System.out.println("The difference Between the given numbers:"+z);   } }
-  // public class Main extends Calculation{
-   public class Calculator_with_Single_Inheritance extends Calculation{
-   public void pro(int x, int y){
-        z=x*y;
-        System.out.println("The product of the given numbers:"+z);    }
-    public static void main(String args[])
-    {int a=20, b=10;
-   Calculator_with_Single_Inheritance demo= new Calculator_with_Single_Inheritance();
-    demo.add(a,b);
-    demo.sub(a,b);
-    demo.pro(a,b);
-    }
-}
+class Calculation {
+ int z;
+ public void add(int x, int y) {
+ z = x + y;
+ System.out.println("The sum of the given numbers:"+z);
+ }
+ public void sub(int x, int y) {
+ z = x - y;
+ System.out.println("The difference between the given numbers:"+z);
+ }
+ }
+ public class Calculator_with_Single_Inheritance extends Calculation {
+ public void pro(int x, int y) {
+ z = x * y;
+ System.out.println("The product of the given numbers:"+z);
+ }
+ public void divi(int x, int y) {
+ z = x / y;
+ System.out.println("The qoutient of the given numbers:"+z);
+ }
+ public void mod(int x, int y) {
+ z = x % y;
+ System.out.println("The modulas of the given numbers:"+z);
+ }
+ public static void main(String args[]) {
+ int a =45, b = 10;
+ Calculator_with_Single_Inheritance demo = new Calculator_with_Single_Inheritance();
+ demo.add(a, b);
+ demo.sub(a, b);
+ demo.pro(a, b);
+ demo.divi(a, b);
+ demo.mod(a, b);
+ }
+ }
